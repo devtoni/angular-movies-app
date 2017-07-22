263a9dd2a0ffd3ad807a0422179a5ac7861bc752
devtoni <devtonirm@gmail.com>
2017-07-22 21:49:31 +0200

    added youtube embed

--- js/routes/movie/controller.js
+++ js/routes/movie/controller.js
@@ -1,11 +1,25 @@
 
 angular.module('angularMovies')
-  .controller('movieCtrl', function ($scope, $routeParams, moviesDataServices) {
-  	$scope.idMovie = $routeParams.id
-  	moviesDataServices.infoMovie($scope.idMovie)
-  		.then(function (movie) {
-  			console.log(movie)
-  			$scope.movie = movie.data
-  			$scope.genres = movie.data.genres.map(genre => genre.name).join(', ')
-  		})
+  .controller('movieCtrl', function ($scope, $routeParams, $sce, moviesDataServices) {
+    $scope.idMovie = $routeParams.id
+    moviesDataServices.infoMovie($scope.idMovie)
+      .then(function (movieInfo) {
+        console.log(movieInfo)
+        $scope.movie = movieInfo.data
+        $scope.genres = movieInfo.data.genres.map(genre => genre.name).join(', ')
+        $scope.videoLink = 'https://www.youtube.com/embed/' + $scope.movie.videos.results['0'].key
+        $scope.youtube = $sce.trustAsResourceUrl($scope.videoLink)
+      })
+
+    $scope.video = false
+    $scope.back = function () {
+      window.history.back()
+    }
+    $scope.watchVideo = function () {
+      if ($scope.video) {
+        $scope.video = false
+      } else {
+        $scope.video = true
+      }
+    }
   })
